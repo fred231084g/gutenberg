4b16d719aad11ef8673c3955df27d617f61ae2ca
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-09-16 18:25:41 +0200

    Update modal

--- packages/block-editor/src/hooks/block-bindings.js
+++ packages/block-editor/src/hooks/block-bindings.js
@@ -200,7 +200,7 @@ function EditableBlockBindingsPanelItems( { attributes, bindings, sources } ) {
 	const isMobile = useViewportMatch( 'medium', '<' );
 	const [ modalState, setModalState ] = useState( null );
 
-	const handleOpenModal = ( attribute, sourceKey ) => {
+	const handleOpenModal = ( { attribute, sourceKey } ) => {
 		setModalState( { attribute, sourceKey } );
 	};
 
