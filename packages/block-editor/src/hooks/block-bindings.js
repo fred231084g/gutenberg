f8f58150cae0f5de068e21d16af4672210f426e2
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-09-17 16:35:08 +0200

    Fix useSelect warning

--- packages/block-editor/src/hooks/block-bindings.js
+++ packages/block-editor/src/hooks/block-bindings.js
@@ -266,6 +266,7 @@ export const BlockBindingsPanel = ( { name: blockName, metadata } ) => {
 
 	// Use useSelect to ensure sources are updated whenever there are updates in block context
 	// or when underlying data changes.
+	const _sources = {};
 	const { sources, canUpdateBlockBindings } = useSelect(
 		( select ) => {
 			const _canUpdateBlockBindings =
@@ -278,13 +279,12 @@ export const BlockBindingsPanel = ( { name: blockName, metadata } ) => {
 				};
 			}
 
-			const _sources = {};
 			const registeredSources = getBlockBindingsSources();
 			Object.entries( registeredSources ).forEach(
 				( [ sourceName, { editorUI, usesContext, label } ] ) => {
 					if ( editorUI ) {
 						// Populate context.
-						const context = { ...blockContext };
+						const context = {};
 						if ( usesContext?.length ) {
 							for ( const key of usesContext ) {
 								context[ key ] = blockContext[ key ];
