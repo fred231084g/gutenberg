7ec0025b06a32d439b81a078f35eb81faf34ce62
fred231084g <freddy1984govoni@gmail.com>
2025-09-26 06:07:11 +0200

    Delete packages/editor/src/components directory

--- packages/editor/src/components/collab-sidebar/utils.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sanitizes a comment string by removing non-printable ASCII characters.
- *
- * @param {string} str - The comment string to sanitize.
- * @return {string} - The sanitized comment string.
- */
-export function sanitizeCommentString( str ) {
-	return str.trim();
-}
